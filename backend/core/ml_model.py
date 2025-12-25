7fc487fc4d96a21566e3281e4b473e8f0fbcd1bc
Akshatha-14 <kakshathashetty6@gmail.com>
2025-12-25 08:38:50 +0530

    Delete backend directory

--- backend/core/ml_model.py
+++
@@ -1,9 +0,0 @@
-import lightgbm as lgb
-import joblib
-import os
-
-MODEL_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../ml_models/lgb_ranker.txt")
-FEATURES_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../ml_models/feature_cols.pkl")
-
-recommendation_model = lgb.Booster(model_file=MODEL_PATH)
-feature_cols = joblib.load(FEATURES_PATH)
